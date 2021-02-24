0b281d90cb158a91b0347cd0a9eb40c2533352ab
Isaac <isaacheye@unisabana.edu.co>
2021-02-23 19:01:16 -0500

    creating pestanias

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import logo from './logo.svg';
+import Pestania from './Pestania';
 import './App.css';
 
-function App() {
+
+var App = ()=>
+{
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='contenedorPestanias'>
+        <Pestania titulo='Our services'/> 
+        <Pestania titulo='To do'/>
+        <Pestania titulo='Did you know'/>  
+      </div>
+      
     </div>
   );
 }
+
+
+
+
 
 export default App;
---
+++ src/Pestania.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+
+var Pestania = (props) =>
+{
+  return(
+    <div className = "pestania ">
+      <div className ='headerPestania'>
+        <h3 className='tituloPestania'>{props.titulo}</h3>
+      </div>
+      <div className ='contenidoPestania'>
+        
+      </div>
+    </div>    
+  );
+}
+export default Pestania;
