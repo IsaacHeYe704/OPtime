9798ba5cdb6f9300fdccbd38242d1a1ef4b9c113
Isaac Herrera <isaacheye@unisabana.edu.co>
2021-02-25 17:56:29 -0500

    adding the header

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import Pestania from './Pestania';
 import './App.css';
 import "./Pestania.css";
+import Header from "./header";
 
 
 var App = ()=>
 {
   return (
     <div className="App">
+      <Header />
       <div className='contenedorPestanias'>
         <Pestania titulo='Our services' /> 
         <Pestania titulo='To do' id='toDo'/>
---
+++ src/header.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Header.css";
+
+
+var Header = (props) =>
+{
+  return(
+    <div className = "Header" >
+        <div className='Branding'>
+            <img src="../logoOP.png" alt="MDN" />
+            <h1 >OP <h4>timize</h4>TIME</h1>
+        </div>  
+        <div className="profileContainer">
+            <div><button><i class="fa fa-bars fa-3x" aria-hidden="true"></i></button><i class="fas fa-user fa-5x"></i> </div>
+            <div><h4>Bienvenido Isaac</h4></div>
+            
+        </div>  
+    </div>    
+  );
+}
+export default Header;
